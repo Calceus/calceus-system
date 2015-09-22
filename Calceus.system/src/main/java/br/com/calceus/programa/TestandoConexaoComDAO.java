f5cca78e2b9fa4e3cc9d13eb15089d046bbc2c19
heisenberg <laercio_ferracini@yahoo.com.br>
2015-09-21 23:02:25 -0300

    Mudanças TestandoDAO

--- Calceus.system/src/main/java/br/com/calceus/programa/TestandoConexaoComDAO.java
+++ Calceus.system/src/main/java/br/com/calceus/programa/TestandoConexaoComDAO.java
@@ -7,7 +7,7 @@ import br.com.calceus.modelo.Cliente;
 
 public class TestandoConexaoComDAO {
 	public static void main(String[] args) throws SQLException {
-		 Cliente cliente = new Cliente(1,1,'s',20);
+		 Cliente cliente = new Cliente(1,"Ativo",'s',1);
 		 try {
 		 ClienteDAO clienteDAO = new ClienteDAO();
 		 clienteDAO.adicionaCliente(cliente);
@@ -18,7 +18,7 @@ public class TestandoConexaoComDAO {
 		ClienteDAO cliente2 = new ClienteDAO();
 		Cliente cliente1 = new Cliente();
 		cliente1 = cliente2.consultarCliente(1);
-		cliente1.mostra();
+		cliente1.getIdCliente();
 
 	}
 }
