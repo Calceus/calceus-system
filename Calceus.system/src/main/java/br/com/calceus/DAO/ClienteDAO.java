17889ee40e1422c70c9a9ae1ca1a41ea835539a6
heisenberg <laercio_ferracini@yahoo.com.br>
2015-09-21 23:00:31 -0300

    Mudanças no ClienteDAO

--- Calceus.system/src/main/java/br/com/calceus/DAO/ClienteDAO.java
+++ Calceus.system/src/main/java/br/com/calceus/DAO/ClienteDAO.java
@@ -13,39 +13,38 @@ public class ClienteDAO {
 
 	public ClienteDAO() throws SQLException {
 		try {
-			this.conn = GerenciadorDeConexoes.getConnection("mysql", "localhost", "calceus", "root", "camila2015");
+			this.conn = GerenciadorDeConexoes.getConnection("localhost", "calceus");
 		} catch (Exception e) {
 			// throw new Exception("Erro " + e.getMessage());
-			System.out.println("Erro durante a conex√£o");
+			System.out.println("Erro durante a conex„o: " + e.getMessage()); //SEMPRE PEGAR A MENSAGEM DE ERRO.
 		}
 	}
 
 	public void adicionaCliente(Cliente cliente) {
 		PreparedStatement pps = null;
-		Connection conn = null;
+		
 		if (cliente == null) {
-			System.out.println("O valor passado n√£o pode ser nulo");
+			System.out.println("O valor passado n„o pode ser nulo");
 		}
 		try {
-			String comando = "INSERT INTO Cliente(idCliente,tipoCliente,status,idPessoa)" + "values(?,?,?,?)";
-			conn = this.conn;
+			String comando = "INSERT INTO Cliente(idCliente,tipoCliente,status,Pessoa_idPessoa)" + "values(?,?,?,?)";
 			pps = conn.prepareStatement(comando);
 			pps.setInt(1, cliente.getIdCliente());
-			pps.setInt(1, cliente.getTipoCliente());
-			pps.setLong(3, cliente.getStatus());
+			pps.setString(2, cliente.getTipoCliente());
+			pps.setString(3, String.valueOf(cliente.getStatus()));
 			pps.setInt(4, cliente.getIdPessoa());
 			pps.execute();
 			pps.close();
 		} catch (SQLException e) {
-			System.out.println("Erro ao inserir dados no banco");
-		}finally {
+			System.out.println("Erro ao inserir dados no banco: " + e.getMessage());
+		} finally {
 			try {
 				conn.close();
 				System.out.println("Dados inseridos com sucesso");
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
-//				e.printStackTrace();
-				System.out.println("Erro ao fechar a conex√£o com banco!");
+				// e.printStackTrace();
+				System.out.println("Erro ao fechar a conex„o com banco! " + e.getMessage());
 			}
 		}
 
@@ -56,28 +55,33 @@ public class ClienteDAO {
 
 	public void excluirCliente() {
 	}
+
 	public Cliente consultarCliente(int idCliente) throws SQLException {
 		PreparedStatement pps = null;
-		Connection conn = null; 
-		ResultSet rs = null; 
+		Connection conn = null;
+		ResultSet rs = null;
 		Cliente cliente = null;
 		try {
 			conn = this.conn;
-			pps = conn.prepareStatement("selec * from Clientes where id = ?");
+			pps = conn.prepareStatement("select * from Cliente where idCliente = ?");// FALTANDO
+																				// O
+																				// T
+																				// NO
+																				// SELECT
 			pps.setInt(1, idCliente);
 			rs = pps.executeQuery();
-			if(! rs.next()){
-				throw new Exception("N√£o foi encontrado nenhum resultado para o ID:" + idCliente);
+			if (!rs.next()) {
+				throw new Exception("N„o foi encontrado nenhum resultado para o ID:" + idCliente);
 			}
 			int id = rs.getInt(1);
-			int tipo = rs.getInt(2);
+			String tipo = rs.getString(2);
 			char status = (char) rs.getLong(3);
 			int idPessoa = rs.getInt(4);
-			
-			cliente =  new Cliente(id, tipo, status,idPessoa); 
+
+			cliente = new Cliente(id, tipo, status, idPessoa);
 		} catch (Exception e) {
-			System.out.println("erro ao executar opera√ß√£o");
-		}finally {
+			System.out.println("Erro ao executar operaÁ„o: " + e.getMessage());
+		} finally {
 			conn.close();
 		}
 		return cliente;
