82fcafa9751ee59459fd4bc53877e64fa9b4cf08
Welington Santos <wellingthonsan@gmail.com>
2015-09-23 18:20:34 -0300

    Criação do pacote interfaces

---
+++ Calceus.system/src/main/java/br/com/calceus/interfacesDAO/ClienteDAO.java
@@ -0,0 +1,5 @@
+package br.com.calceus.interfacesDAO;
+
+public class ClienteDAO {
+
+}
---
+++ Calceus.system/src/main/java/br/com/calceus/interfacesDAO/FuncionarioDAO.java
@@ -0,0 +1,9 @@
+package br.com.calceus.interfacesDAO;
+
+import br.com.calceus.modelo.Cliente;
+
+public interface FuncionarioDAO {
+     
+	public Cliente adicionaCliente(Cliente cliente);
+	
+}
