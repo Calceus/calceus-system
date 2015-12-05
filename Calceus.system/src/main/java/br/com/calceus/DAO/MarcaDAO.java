115bf64e0d2b0982d6ff6c8515faffc6dc61f115
WelingtonSantos <wellingthonsan@gmail.com>
2015-12-05 00:31:19 -0200

    Atualizando as DAOs

--- Calceus.system/src/main/java/br/com/calceus/DAO/MarcaDAO.java
+++ Calceus.system/src/main/java/br/com/calceus/DAO/MarcaDAO.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.calceus.conexao.ConnectionPool;
 import br.com.calceus.conexao.GerenciadorDeConexoes;
 import br.com.calceus.modelo.Marca;
 
@@ -15,52 +16,45 @@ public class MarcaDAO {
 
 	public boolean salvar(Marca marca) throws SQLException {
 		String sql = "INSERT INTO marca (marca) VALUES (?)";
-		try(Connection conn = GerenciadorDeConexoes.getConnection()){
-		
+		try (Connection conn = GerenciadorDeConexoes.getConnection()) {
 
-			try (PreparedStatement ps = conn.prepareStatement(sql)){
+			try (PreparedStatement ps = conn.prepareStatement(sql)) {
 				ps.setString(1, marca.getMarca());
 				ps.execute();
-			
+
 				return true;
 			} catch (SQLException e) {
-			System.out.println("Erro ao inserir no banco " + e.getMessage());
-			return false;
-	
+				System.out
+						.println("Erro ao inserir no banco " + e.getMessage());
+				return false;
+
 			}
 		}
 	}
 
-	public List<Marca> listar() {
-		Connection conn = GerenciadorDeConexoes.getConnection();
-		String sql = "SELECT * FROM Marca";
-		Statement st = null;
-		ResultSet rs = null;
+	public List<Marca> listar() throws SQLException {
 		Marca marca = null;
 		List<Marca> marcas = new ArrayList<Marca>();
-		
-		try{
-			st = conn.createStatement();
-			rs = st.executeQuery(sql);
-			while(rs.next()){
-				
-				marca = new Marca();
-				marca.setIdMarca(rs.getInt("idMarca"));
-				marca.setMarca(rs.getString("marca"));
-				
-				marcas.add(marca);
-			}
-			st.close();
-			rs.close();
-			return marcas;
-		}catch(SQLException e){
-			System.out.println("Erro na pesquisa de marcas: "+e.getMessage());
-			return null;
-		}finally {
-			try{
-				conn.close();
-			}catch(Exception e){
-				System.out.println("Erro ao fechar operações de consulta. Mensagem: " + e.getMessage());
+		String sql = "SELECT * FROM Marca";
+		try (Connection conn = new ConnectionPool().getConnection()) {
+
+			try (Statement st = conn.createStatement()) {
+				try (ResultSet rs = st.executeQuery(sql)) {
+					while (rs.next()) {
+
+						marca = new Marca();
+						marca.setIdMarca(rs.getInt("idMarca"));
+						marca.setMarca(rs.getString("marca"));
+
+						marcas.add(marca);
+					}
+				}
+
+				return marcas;
+			} catch (SQLException e) {
+				System.out.println("Erro na pesquisa de marcas: "
+						+ e.getMessage());
+				return null;
 			}
 		}
 	}
