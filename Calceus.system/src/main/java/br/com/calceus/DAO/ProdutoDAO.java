2653a88f7645c09981a9eef47cdc33be82f20f68
WelingtonSantos <wellingthonsan@gmail.com>
2015-12-05 19:22:25 -0200

    Adição da classe ProdutoDAO, e alteração da classe produto.

---
+++ Calceus.system/src/main/java/br/com/calceus/DAO/ProdutoDAO.java
@@ -0,0 +1,37 @@
+package br.com.calceus.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.calceus.conexao.ConnectionPool;
+import br.com.calceus.modelo.Produto;
+
+public class ProdutoDAO {
+	public boolean adicionaProduto(Produto produto) {
+		String sql = "INSERT INTO produto(idProduto, idFornecedor, idCategoria, nomeProduto, quantidade, preco, genero, idItemCompra, idMarca) VALUES (?,?,?,?,?,?,?,?,?)";
+		if (produto == null) {
+			System.out.println("Valor passado nao pode ser nullo");
+		}
+		try (Connection conexao = new ConnectionPool().getConnection()) {
+			try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
+				stmt.setInt(1, produto.getIdProduto());
+				stmt.setInt(2, produto.getIdFornecedor());
+				stmt.setInt(3, produto.getIdCategoria());
+				stmt.setString(4, produto.getNomeProduto());
+				stmt.setInt(5, produto.getQuantidade());
+				stmt.setDouble(6, produto.getValor());
+				stmt.setLong(7, produto.getGenero());
+				stmt.setInt(8, produto.getIdItemProduto());
+				stmt.setInt(9, produto.getIdMarca());
+				stmt.execute();
+				return true;
+			}
+
+		} catch (SQLException e) {
+			System.out.println("Erro" + e.getMessage());
+			return false;
+		}
+
+	}
+}
--- Calceus.system/src/main/java/br/com/calceus/modelo/Produto.java
+++ Calceus.system/src/main/java/br/com/calceus/modelo/Produto.java
@@ -10,21 +10,38 @@ public class Produto {
 	private double valor;
 	private int idCategoria;
 	private int idFornecedor;
+	private int idItemProduto;
+	private int idMarca;
 	private char genero;
-	
+
 	public Produto() {
-		
+
 	}
 
-	public Produto(int idProduto, String nomeProduto, int quantidade, double valor) {
+	public Produto(int idProduto, String nomeProduto, int quantidade,
+			double valor) {
 		this.idProduto = idProduto;
 		this.nomeProduto = nomeProduto;
 		this.quantidade = quantidade;
 		this.valor = valor;
 
 	}
-	
-	
+
+	public Produto(int idProduto, String nomeProduto, int idCategoria,
+			int idFornecedor, int idItemProduto, int quantidade, double valor, int idMarca) {
+		this.idProduto = idProduto;
+		this.nomeProduto = nomeProduto;
+		this.idCategoria = idCategoria;
+		this.idFornecedor = idFornecedor;
+		this.idItemProduto = idItemProduto;
+		this.quantidade = quantidade;
+		this.valor = valor;
+		this.idMarca = idMarca;
+
+	}
+
+
+
 	public Produto(int idProduto2) {
 		this.idProduto = idProduto2;
 	}
@@ -57,30 +74,40 @@ public class Produto {
 		return idCategoria;
 	}
 
+	public int getIdItemProduto() {
+		return idItemProduto;
+	}
+	public int getIdMarca() {
+		return idMarca;
+	}
 	public boolean verificarDisponibilidade(int idProduto) {
 		return false;
 	}
-	 
-	public List<Produto> consultarProdutosEmPromocao(List<Produto> produtosEmPromocao) {
+
+	public List<Produto> consultarProdutosEmPromocao(
+			List<Produto> produtosEmPromocao) {
 		return null;
 	}
+
 	public boolean cadastrarProduto() {
 		return false;
 	}
-	 
+
 	public List<Produto> consultarProdutos() {
 		return null;
 	}
-	 
+
 	public Produto alterarProduto() {
 		return null;
 	}
-	 
+
 	public boolean excluirProduto() {
 		return false;
 	}
-	 
+
 	public boolean atualizarEstoque() {
 		return false;
 	}
+
+	
 }
